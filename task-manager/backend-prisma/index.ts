eda7c114f19cda58f0a671c2abcf37fa1f742554
Cyril Lepagnot <cyril@lepagnot.fr>
2023-01-03 07:20:55 +0100

    Add a Subscription sample with WebSocket server & Redis as pubSub system

--- task-manager/backend-prisma/index.ts
+++ task-manager/backend-prisma/index.ts
@@ -1,19 +1,42 @@
 import "reflect-metadata";
-import { buildSchema } from "type-graphql";
+import * as TypeGraphQL from "type-graphql";
 import { ApolloServer } from "apollo-server";
 import { PrismaClient } from "@prisma/client";
+import { WebSocketServer } from 'ws';
 
-import {
-  resolvers
-} from "./prisma/generated/type-graphql";
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { resolvers as generatedResolvers } from "./prisma/generated/type-graphql";
+
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 interface Context {
   prisma: PrismaClient;
 }
 
+const pubSub = new RedisPubSub({
+  messageEventName: 'messageBuffer',
+  pmessageEventName: 'pmessageBuffer',
+});
+
+class SubscriptionResolver {
+  @TypeGraphQL.Subscription({
+    topics: ["NOTIFICATIONS", "ERRORS"],
+    subscribe: () => pubSub.asyncIterator('NOTIFICATIONS'),
+  })
+  hello(): String {
+    return 'hello';
+  };
+}
+
+const resolvers = [
+  ...generatedResolvers,
+  SubscriptionResolver,
+] as TypeGraphQL.NonEmptyArray<Function>;
+
 async function main() {
-  const schema = await buildSchema({
+  const schema = await TypeGraphQL.buildSchema({
     resolvers,
+    pubSub,
     emitSchemaFile: "./generated-schema.graphql",
     validate: false,
   });
@@ -22,7 +45,14 @@ async function main() {
   const server = new ApolloServer({
     schema,
     context: (): Context => ({ prisma }),
+    introspection: true,
   });
+  const wsServer = new WebSocketServer({
+    server: server.httpServer,
+    path: '/subscriptions',
+  });
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const { port } = await server.listen(4000);
   console.log(`GraphQL is listening on ${port}!`);
 }
